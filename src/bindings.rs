14caeda241f3fc1f6672197a6db92dfebcb285cc
Chris Ha <hac541309@gmail.com>
2023-08-01 12:36:10 +0900

    Update bindings.rs
    change version number in the bindings so tests pass

--- src/bindings.rs
+++ src/bindings.rs
@@ -6,8 +6,9 @@ use ::core::ffi::c_void;
 
 pub const XXH_VERSION_MAJOR: u32 = 0;
 pub const XXH_VERSION_MINOR: u32 = 8;
-pub const XXH_VERSION_RELEASE: u32 = 1;
-pub const XXH_VERSION_NUMBER: u32 = XXH_VERSION_MAJOR*100*100 + XXH_VERSION_MINOR*100 + XXH_VERSION_RELEASE;
+pub const XXH_VERSION_RELEASE: u32 = 2;
+pub const XXH_VERSION_NUMBER: u32 =
+    XXH_VERSION_MAJOR * 100 * 100 + XXH_VERSION_MINOR * 100 + XXH_VERSION_RELEASE;
 pub const XXH3_SECRET_SIZE_MIN: libc::c_int = 136;
 pub const XXH_errorcode_XXH_OK: XXH_errorcode = 0;
 pub const XXH_errorcode_XXH_ERROR: XXH_errorcode = 1;
@@ -16,12 +17,12 @@ pub type XXH32_hash_t = u32;
 #[repr(C)]
 #[derive(Debug, Copy, Clone)]
 pub struct XXH32_state_s {
-   total_len_32: XXH32_hash_t,
-   large_len: XXH32_hash_t,
-   v: [XXH32_hash_t; 4],
-   mem32: [XXH32_hash_t; 4],
-   memsize: XXH32_hash_t,
-   reserved: XXH32_hash_t,
+    total_len_32: XXH32_hash_t,
+    large_len: XXH32_hash_t,
+    v: [XXH32_hash_t; 4],
+    mem32: [XXH32_hash_t; 4],
+    memsize: XXH32_hash_t,
+    reserved: XXH32_hash_t,
 }
 pub type XXH32_state_t = XXH32_state_s;
 #[repr(C)]
@@ -33,12 +34,12 @@ pub type XXH64_hash_t = u64;
 #[repr(C)]
 #[derive(Debug, Copy, Clone)]
 pub struct XXH64_state_s {
-   total_len: XXH64_hash_t,
-   v: [XXH64_hash_t; 4],
-   mem64: [XXH64_hash_t; 4],
-   memsize: XXH32_hash_t,
-   reserved32: XXH32_hash_t,
-   reserved64: XXH64_hash_t,
+    total_len: XXH64_hash_t,
+    v: [XXH64_hash_t; 4],
+    mem64: [XXH64_hash_t; 4],
+    memsize: XXH32_hash_t,
+    reserved32: XXH32_hash_t,
+    reserved64: XXH64_hash_t,
 }
 pub type XXH64_state_t = XXH64_state_s;
 #[repr(C)]
@@ -98,7 +99,11 @@ extern "C" {
     pub fn XXH32_freeState(statePtr: *mut XXH32_state_t) -> XXH_errorcode;
     pub fn XXH32_copyState(dst_state: *mut XXH32_state_t, src_state: *const XXH32_state_t);
     pub fn XXH32_reset(statePtr: *mut XXH32_state_t, seed: XXH32_hash_t) -> XXH_errorcode;
-    pub fn XXH32_update(statePtr: *mut XXH32_state_t, input: *const c_void, length: usize) -> XXH_errorcode;
+    pub fn XXH32_update(
+        statePtr: *mut XXH32_state_t,
+        input: *const c_void,
+        length: usize,
+    ) -> XXH_errorcode;
     pub fn XXH32_digest(statePtr: *const XXH32_state_t) -> XXH32_hash_t;
     pub fn XXH32_canonicalFromHash(dst: *mut XXH32_canonical_t, hash: XXH32_hash_t);
     pub fn XXH32_hashFromCanonical(src: *const XXH32_canonical_t) -> XXH32_hash_t;
@@ -108,39 +113,113 @@ extern "C" {
     pub fn XXH64_freeState(statePtr: *mut XXH64_state_t) -> XXH_errorcode;
     pub fn XXH64_copyState(dst_state: *mut XXH64_state_t, src_state: *const XXH64_state_t);
     pub fn XXH64_reset(statePtr: *mut XXH64_state_t, seed: XXH64_hash_t) -> XXH_errorcode;
-    pub fn XXH64_update(statePtr: *mut XXH64_state_t, input: *const c_void, length: usize,) -> XXH_errorcode;
+    pub fn XXH64_update(
+        statePtr: *mut XXH64_state_t,
+        input: *const c_void,
+        length: usize,
+    ) -> XXH_errorcode;
     pub fn XXH64_digest(statePtr: *const XXH64_state_t) -> XXH64_hash_t;
     pub fn XXH64_canonicalFromHash(dst: *mut XXH64_canonical_t, hash: XXH64_hash_t);
     pub fn XXH64_hashFromCanonical(src: *const XXH64_canonical_t) -> XXH64_hash_t;
     ///xxh3
     pub fn XXH3_64bits(data: *const c_void, len: usize) -> XXH64_hash_t;
-    pub fn XXH3_64bits_withSeed(data: *const c_void, len: usize, seed: XXH64_hash_t) -> XXH64_hash_t;
-    pub fn XXH3_64bits_withSecret(data: *const c_void, len: usize, secret: *const c_void, secretSize: usize,) -> XXH64_hash_t;
+    pub fn XXH3_64bits_withSeed(
+        data: *const c_void,
+        len: usize,
+        seed: XXH64_hash_t,
+    ) -> XXH64_hash_t;
+    pub fn XXH3_64bits_withSecret(
+        data: *const c_void,
+        len: usize,
+        secret: *const c_void,
+        secretSize: usize,
+    ) -> XXH64_hash_t;
     pub fn XXH3_createState() -> *mut XXH3_state_t;
     pub fn XXH3_freeState(statePtr: *mut XXH3_state_t) -> XXH_errorcode;
     pub fn XXH3_copyState(dst_state: *mut XXH3_state_t, src_state: *const XXH3_state_t);
     pub fn XXH3_64bits_reset(statePtr: *mut XXH3_state_t) -> XXH_errorcode;
-    pub fn XXH3_64bits_reset_withSeed(statePtr: *mut XXH3_state_t, seed: XXH64_hash_t) -> XXH_errorcode;
-    pub fn XXH3_64bits_reset_withSecret(statePtr: *mut XXH3_state_t, secret: *const c_void, secretSize: usize) -> XXH_errorcode;
-    pub fn XXH3_64bits_update(statePtr: *mut XXH3_state_t, input: *const c_void, length: usize) -> XXH_errorcode;
+    pub fn XXH3_64bits_reset_withSeed(
+        statePtr: *mut XXH3_state_t,
+        seed: XXH64_hash_t,
+    ) -> XXH_errorcode;
+    pub fn XXH3_64bits_reset_withSecret(
+        statePtr: *mut XXH3_state_t,
+        secret: *const c_void,
+        secretSize: usize,
+    ) -> XXH_errorcode;
+    pub fn XXH3_64bits_update(
+        statePtr: *mut XXH3_state_t,
+        input: *const c_void,
+        length: usize,
+    ) -> XXH_errorcode;
     pub fn XXH3_64bits_digest(statePtr: *const XXH3_state_t) -> XXH64_hash_t;
     pub fn XXH3_128bits(data: *const c_void, len: usize) -> XXH128_hash_t;
-    pub fn XXH3_128bits_withSeed(data: *const c_void, len: usize, seed: XXH64_hash_t) -> XXH128_hash_t;
-    pub fn XXH3_128bits_withSecret(data: *const c_void, len: usize, secret: *const c_void, secretSize: usize) -> XXH128_hash_t;
+    pub fn XXH3_128bits_withSeed(
+        data: *const c_void,
+        len: usize,
+        seed: XXH64_hash_t,
+    ) -> XXH128_hash_t;
+    pub fn XXH3_128bits_withSecret(
+        data: *const c_void,
+        len: usize,
+        secret: *const c_void,
+        secretSize: usize,
+    ) -> XXH128_hash_t;
     pub fn XXH3_128bits_reset(statePtr: *mut XXH3_state_t) -> XXH_errorcode;
-    pub fn XXH3_128bits_reset_withSeed(statePtr: *mut XXH3_state_t, seed: XXH64_hash_t) -> XXH_errorcode;
-    pub fn XXH3_128bits_reset_withSecret(statePtr: *mut XXH3_state_t, secret: *const c_void, secretSize: usize) -> XXH_errorcode;
-    pub fn XXH3_128bits_update(statePtr: *mut XXH3_state_t, input: *const c_void, length: usize) -> XXH_errorcode;
+    pub fn XXH3_128bits_reset_withSeed(
+        statePtr: *mut XXH3_state_t,
+        seed: XXH64_hash_t,
+    ) -> XXH_errorcode;
+    pub fn XXH3_128bits_reset_withSecret(
+        statePtr: *mut XXH3_state_t,
+        secret: *const c_void,
+        secretSize: usize,
+    ) -> XXH_errorcode;
+    pub fn XXH3_128bits_update(
+        statePtr: *mut XXH3_state_t,
+        input: *const c_void,
+        length: usize,
+    ) -> XXH_errorcode;
     pub fn XXH3_128bits_digest(statePtr: *const XXH3_state_t) -> XXH128_hash_t;
     pub fn XXH128_isEqual(h1: XXH128_hash_t, h2: XXH128_hash_t) -> libc::c_int;
     pub fn XXH128_cmp(h128_1: *const c_void, h128_2: *const c_void) -> libc::c_int;
     pub fn XXH128_canonicalFromHash(dst: *mut XXH128_canonical_t, hash: XXH128_hash_t);
     pub fn XXH128_hashFromCanonical(src: *const XXH128_canonical_t) -> XXH128_hash_t;
     pub fn XXH128(data: *const c_void, len: usize, seed: XXH64_hash_t) -> XXH128_hash_t;
-    pub fn XXH3_generateSecret(secretBuffer: *mut c_void, secretSize: usize, customSeed: *const c_void, customSeedSize: usize) -> XXH_errorcode;
-    pub fn XXH3_generateSecret_fromSeed(secretBuffer: *mut c_void, seed: XXH64_hash_t) -> XXH_errorcode;
-    pub fn XXH3_64bits_withSecretandSeed(data: *const c_void, len: usize, secret: *const c_void, secretSize: usize, seed: XXH64_hash_t) -> XXH64_hash_t;
-    pub fn XXH3_128bits_withSecretandSeed(data: *const c_void, len: usize, secret: *const c_void, secretSize: usize, seed: XXH64_hash_t) -> XXH128_hash_t;
-    pub fn XXH3_64bits_reset_withSecretandSeed(statePtr: *mut XXH3_state_t, secret: *const c_void, secretSize: usize, seed: XXH64_hash_t) -> XXH_errorcode;
-    pub fn XXH3_128bits_reset_withSecretandSeed(statePtr: *mut XXH3_state_t, secret: *const c_void, secretSize: usize, seed: XXH64_hash_t) -> XXH_errorcode;
+    pub fn XXH3_generateSecret(
+        secretBuffer: *mut c_void,
+        secretSize: usize,
+        customSeed: *const c_void,
+        customSeedSize: usize,
+    ) -> XXH_errorcode;
+    pub fn XXH3_generateSecret_fromSeed(
+        secretBuffer: *mut c_void,
+        seed: XXH64_hash_t,
+    ) -> XXH_errorcode;
+    pub fn XXH3_64bits_withSecretandSeed(
+        data: *const c_void,
+        len: usize,
+        secret: *const c_void,
+        secretSize: usize,
+        seed: XXH64_hash_t,
+    ) -> XXH64_hash_t;
+    pub fn XXH3_128bits_withSecretandSeed(
+        data: *const c_void,
+        len: usize,
+        secret: *const c_void,
+        secretSize: usize,
+        seed: XXH64_hash_t,
+    ) -> XXH128_hash_t;
+    pub fn XXH3_64bits_reset_withSecretandSeed(
+        statePtr: *mut XXH3_state_t,
+        secret: *const c_void,
+        secretSize: usize,
+        seed: XXH64_hash_t,
+    ) -> XXH_errorcode;
+    pub fn XXH3_128bits_reset_withSecretandSeed(
+        statePtr: *mut XXH3_state_t,
+        secret: *const c_void,
+        secretSize: usize,
+        seed: XXH64_hash_t,
+    ) -> XXH_errorcode;
 }

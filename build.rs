78f29e9e1eae7eba9358fd246416bcab46a0f568
DoumanAsh <douman@gmx.se>
2024-02-17 11:20:11 +0900

    0.8.6: Correct no_std feature usage

--- build.rs
+++ build.rs
@@ -2,8 +2,8 @@ fn main() {
     let mut cc = cc::Build::new();
 
     cc.include("src/c/");
-    #[cfg(features = "no_std")]
-    cc.define("XXH_NO_STDLIB");
+    #[cfg(feature = "no_std")]
+    cc.define("XXH_NO_STDLIB", None);
 
     cc.file("src/c/xxhash.c");
     cc.warnings(false);
